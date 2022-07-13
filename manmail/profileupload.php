6400f111b0f13a5655bb37548c283e5fe192879f
manish <manish.singh.hestabit@gmail.com>
2022-07-13 16:23:28 +0530

    change in profile upload

--- manmail/profileupload.php
+++ manmail/profileupload.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-
 if ($_SESSION['login']) {
 
     include_once "../php/dbconnect.php";
@@ -14,8 +13,7 @@ if ($_SESSION['login']) {
         $secondemail = $_POST['recoveryemail'];
         $picture = $_FILES['picture']['name'];
         $name_image_temp = $_FILES['picture']['tmp_name'];
-        // move_uploaded_file($name_image_temp, "/var/www/html/project/images/" . $picture);
-        move_uploaded_file($name_image_temp, "../images/" . $picture);
+        move_uploaded_file($name_image_temp, "/var/www/html/project/images/" . $picture);
         $result = $obj->update($firstname, $lastname, $secondemail, $picture, $time);
     }
 
@@ -73,7 +71,7 @@ if ($_SESSION['login']) {
                     </ul>
                 </div>
                 <div class="profile_image" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="../images/<?php echo $_SESSION['picture'] ?>" alt="img">
+                    <img src="http://localhost/project/images/<?php echo $_SESSION['picture'] ?>" alt="img">
                 </div>
             </div>
             </div>
@@ -116,7 +114,7 @@ if ($_SESSION['login']) {
 
                                 <div class="mt-3 iprofileimf">
                                     <span><button type="submit" name="delete" class="btn btn-outline-danger" style="padding:5px 5px 5px 5px ;width:94px"><a href="profileupload.php?updateid=<?php echo $_SESSION['id'] ?>">Delete</a></button></span>
-                                    <input type="file" class="type djkdfjk" name="picture">
+                                    <input type="file" class="type" name="picture">
                                     <img src="http://localhost/project/images/<?php echo $_SESSION['picture'] ?>" alt="" class="" style="width:300px">
 
                                 </div>
