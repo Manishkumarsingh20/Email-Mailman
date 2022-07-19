f2c41c178aa685bf74dacd6057a774cb9d089a6f
manish <manish.singh.hestabit@gmail.com>
2022-07-19 17:44:43 +0530

    nnnnnnnnnn

--- php/forgotpassword.php
+++ php/forgotpassword.php
@@ -1,7 +1,5 @@
 <?php
 
-echo 'lsdkj';
-die;
 if (isset($_POST['reset'])) {
     $second_email = $_POST['email'];
 } else {
@@ -29,7 +27,6 @@ $verifyQuery = $conn->query($sql);
 $fetch_data = mysqli_fetch_assoc($verifyQuery);
 
 
-
 $recoverymail = $fetch_data['secondemail'];
 
 echo $to_mail = $recoverymail;
@@ -38,35 +35,35 @@ die();
 if ($verifyQuery->num_rows > 0) {
     $reset_codes = "UPDATE users SET reset_code = '$code' WHERE username='$second_email' OR email='$second_email'";
     $reset = $conn->query($reset_codes);
+    echo 'Message has been sent, check your email';
 
     $mail = new PHPMailer(true);
 
-    try {
+    // try {
 
-        $mail->send();
-        echo 'Message has been sent, check your email';
+    //     $mail->send();
 
-        $mail->isSMTP();
-        $mail->Host       = 'smtp.gmail.com';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'manishkumarsingh1798@gmail.com';
-        $mail->Password   = 'zyfhcwesddjwkeif';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-        $mail->Port       = 465;
+    //     $mail->isSMTP();
+    //     $mail->Host       = 'smtp.gmail.com';
+    //     $mail->SMTPAuth   = true;
+    //     $mail->Username   = 'manishkumarsingh1798@gmail.com';
+    //     $mail->Password   = 'zyfhcwesddjwkeif';
+    //     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    //     $mail->Port       = 465;
 
-        $mail->setFrom('manishkumarsingh1798@gmail.com', 'Admin');
-        $to_mail = $recoverymail;
-        $mail->addAddress($email);
-        $code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'), 0, 10);
-
+    //     $mail->setFrom('manishkumarsingh1798@gmail.com', 'Admin');
+    //     $to_mail = $recoverymail;
+    //     $mail->addAddress($email);
+    //     $code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'), 0, 10);
 
 
-        $mail->isHTML(true);
-        $mail->Subject = 'Password Reset';
-        $mail->Body    = 'To reset your password click <a href="http://hestalabs.com/tse/mailnam-manish/manmail/newpassword.php?code=' . $code . '">click here </a> </br>Reset your password in a day.';
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    }
+
+    //     $mail->isHTML(true);
+    //     $mail->Subject = 'Password Reset';
+    //     $mail->Body    = 'To reset your password click <a href="http://hestalabs.com/tse/mailnam-manish/manmail/newpassword.php?code=' . $code . '">click here </a> </br>Reset your password in a day.';
+    // } catch (Exception $e) {
+    //     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    // }
 } else {
     echo 'username or email not found';
 }
