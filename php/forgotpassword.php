743cb401d5b5e86784346ee16b297e3c4de26aa8
manish <manish.singh.hestabit@gmail.com>
2022-07-19 19:11:48 +0530

    profile

--- php/forgotpassword.php
+++ php/forgotpassword.php
@@ -36,36 +36,34 @@ if ($to_mail != '') {
     if ($verifyQuery->num_rows > 0) {
 
         $mail = new PHPMailer(true);
-    
-    try {
-        //Server settings
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'manishkumarsingh1798@gmail.com';                     // SMTP username
-        $mail->Password   = 'zyfhcwesddjwkeif';                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-    
-        //Recipients
-        $mail->setFrom('manishkumarsingh1798@gmail.com', 'Admin');
-        $mail->addAddress($to_mail);     // Add a recipient
 
-        $code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'),0,10);
-    
-        // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Password Reset';
-        $mail->Body    = 'To reset your password click <a href="http://hestalabs.com/tse/mailnam-manish/manmail/newpassword.php?code=' . $code . '">click here </a> </br>Reset your password in a day.';
+        try {
+            //Server settings
+            $mail->isSMTP();                                            // Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            $mail->Username   = 'manishkumarsingh1798@gmail.com';                     // SMTP username
+            $mail->Password   = 'zyfhcwesddjwkeif';                               // SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-        $conn->close();
-    
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-    } 
-    } 
+            //Recipients
+            $mail->setFrom('manishkumarsingh1798@gmail.com', 'Admin');
+            $mail->addAddress($to_mail);     // Add a recipient
+
+            $code = substr(str_shuffle('1234567890QWERTYUIOPASDFGHJKLZXCVBNM'), 0, 10);
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Password Reset';
+            $mail->Body    = 'To reset your password click <a href="http://hestalabs.com/tse/mailnam-manish/manmail/newpassword.php?code=' . $code . '">click here </a> </br>Reset your password in a day.';
+            echo 'success';
+            $conn->close();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
     $conn->close();
-}
-else {
+} else {
     echo 'username or email not found';
 }
