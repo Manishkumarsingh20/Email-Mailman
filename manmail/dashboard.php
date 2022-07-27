6bcf757e3d1ac849c3d1ca2a6296ff67e2ffe6fe
manish <manish.singh.hestabit@gmail.com>
2022-07-27 15:59:13 +0530

    nnnnnnnnnn

--- manmail/dashboard.php
+++ manmail/dashboard.php
@@ -110,7 +110,7 @@ if ($_SESSION['login']) {
             <div class="row">
                 <nav id="sidebar" class="d-md-block col-lg-2 col-md-3  sidebar collapse">
                     <div class="position-sticky ">
-                        <ul class="nav flex-column vr">
+                        <ul class="nav flex-column ">
                             <li class="compose">
                                 <button class=btn class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Compose</button>
                             </li>
